b9b774cd7db1b4aeb8020007da365e373844d750
jackalotUVM <gonzalezc9999@gmail.com>
2021-12-28 11:17:12 -0500

    the test for returning an object would initially return a typeof string instead of an object

--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,5 @@
 const indexFile =  require('index');
 
 test("ship factory returns an object", () => {
-    expect(indexFile.shipFactory(3)).toBe(typeof("object"));
+    expect(indexFile.shipFactory(3)).toBe(typeof({}));
 })
