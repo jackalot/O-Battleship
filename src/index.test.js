44002a760cff9c70c5efa5d91d1f2e632e1e254b
jackalotUVM <gonzalezc9999@gmail.com>
2021-12-29 08:42:08 -0500

    rewrote old tests and wrote new tests

--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,11 @@
 const indexFile =  require('index');
 
 test("ship factory returns an object", () => {
-    expect(indexFile.shipFactory(3)).toBe(typeof({}));
+    expect(indexFile.shipFactory([1, 2], [1, 4])).toBe(typeof({}));
+})
+test("ship factory return a length of 6", () => {
+    expect(indexFile.shipFactory({ coord1: [1, 1], coord2: [1, 7]}).toBe({length: 6}));
+})
+test("ship factory will get hit at [1, 1]", () => {
+    expect(indexFile.shipFactory.hit([1, 1], { coord1: [1, 1], coord2: [1, 7]})).toBe(true);
 })
