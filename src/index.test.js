582e8d36e8eb2c2979a86155f1eab1f463247b37
jackalotUVM <gonzalezc9999@gmail.com>
2021-12-29 08:50:49 -0500

    rewrote the first test to send an object

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,7 @@
 const indexFile =  require('index');
 
 test("ship factory returns an object", () => {
-    expect(indexFile.shipFactory([1, 2], [1, 4])).toBe(typeof({}));
+    expect(indexFile.shipFactory({ coord1: [1, 1], coord2: [1, 7]}).toBe(typeof({})));
 })
 test("ship factory return a length of 6", () => {
     expect(indexFile.shipFactory({ coord1: [1, 1], coord2: [1, 7]}).toBe({length: 6}));
