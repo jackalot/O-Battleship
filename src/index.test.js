113b9910cc6489338025bc58e1bf09ad0af430ac
jackalotUVM <gonzalezc9999@gmail.com>
2021-12-29 09:43:04 -0500

    the top test will be the only test to run now

--- src/index.test.js
+++ src/index.test.js
@@ -1,9 +1,9 @@
 const shipFile =  require('./ship');
 
-test("ship factory returns an object", () => {
+test.only("ship factory returns an object", () => {
     expect(shipFile.shipFactory({ coord1: [1, 1], coord2: [1, 7]}).toBe(typeof({})));
 })
-test.only("ship factory return a length of 6", () => {
+test("ship factory return a length of 6", () => {
     expect(shipFile({ coord1: [1, 1], coord2: [1, 7]}).toBe({length: 6}));
 })
 test("ship factory will get hit at [1, 1]", () => {
