746007eb4978aefde063c17c8eeb15f3b8c890d0
jackalotUVM <gonzalezc9999@gmail.com>
2021-12-29 08:47:42 -0500

    added new test for if the ship will sink

--- src/index.test.js
+++ src/index.test.js
@@ -9,3 +9,6 @@ test("ship factory return a length of 6", () => {
 test("ship factory will get hit at [1, 1]", () => {
     expect(indexFile.shipFactory.hit([1, 1], { coord1: [1, 1], coord2: [1, 7]})).toBe(true);
 })
+test("ship factory will not sink", () => {
+    expect(indexFile.shipFactory.sink().toBe(false));
+})
