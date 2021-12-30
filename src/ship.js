e11d24c5ef852eb57d2d940fe108e01487a7357e
jackalotUVM <gonzalezc9999@gmail.com>
2021-12-30 11:03:00 -0500

    changed the syntax used for shipFactory so it looks more like a factory function

--- src/ship.js
+++ src/ship.js
@@ -1,5 +1,5 @@
-function shipFactory(coord1, coord2) {
-  const lengthMeasurer = (newCoord1, newCoord2) => {
+const shipFactory = (coord1, coord2) => {
+  this.lengthMeasurer = (newCoord1, newCoord2) => {
     const yAxis = newCoord2.x - newCoord1.x;
     const xAxis = newCoord2.y - newCoord1.y;
     if (xAxis === 0) {
@@ -10,6 +10,6 @@ function shipFactory(coord1, coord2) {
     }
     return console.error('error');
   };
-  return { coord1, coord2, lengthMeasurer };
-}
+  return { coord1, coord2 };
+};
 module.exports = shipFactory;
