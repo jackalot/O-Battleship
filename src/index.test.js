188383ae278702c5049aeb5c8514d83aafeb319e
jackalotUVM <gonzalezc9999@gmail.com>
2021-12-30 10:42:37 -0500

    added console.log, also added an array accessor

--- src/index.test.js
+++ src/index.test.js
@@ -1,11 +1,11 @@
 const shipFile = require('./ship');
 const { TestWatcher } = require("jest");
-
+console.log(shipFile.lengthMeasurer);
 test("ship factory returns an object", () => {
     expect(shipFile( coord1 = [1, 1], coord2 = [1, 7])).toStrictEqual({ coord1: [1, 1], coord2: [1, 7]});
 })
 test.only("ship factory return a length of 6", () => {
-    expect(shipFile.lengthMeasurer( coord1 = {x: 1, y: 1}, coord2 = {x: 1, y: 7})).toBe(6);
+    expect(shipFile[0].lengthMeasurer( coord1 = {x: 1, y: 1}, coord2 = {x: 1, y: 7})).toBe(6);
 })
 test("ship factory will get hit at [1, 1]", () => {
     expect(shipFile.shipFactory.hit([1, 1], { coord1: [1, 1], coord2: [1, 7]})).toBe(true);
