763079208cbd8d3d9f59dd68212e99bb464e70ea
jackalotUVM <gonzalezc9999@gmail.com>
2021-12-30 08:48:45 -0500

    added function lengthMeasurer

--- src/ship.js
+++ src/ship.js
@@ -1,4 +1,12 @@
 function shipFactory(coord1, coord2) {
-  return { coord1, coord2 };
+  function lengthMeasurer() {
+    let xAxis = 0;
+    for (let i = coord1[0]; i < coord2[0]; i++) {
+      xAxis += 1;
+    }
+    return xAxis;
+  }
+  const length = lengthMeasurer();
+  return { coord1, coord2, length };
 }
 module.exports = shipFactory;
