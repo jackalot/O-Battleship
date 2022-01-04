f76ce9b57cdfe429b1bf17469393b8c89ad0edd9
jackalotUVM <gonzalezc9999@gmail.com>
2022-01-04 10:53:52 -0500

    removed .only, added a skip to last test, added a new test for y level hits

--- src/index.test.js
+++ src/index.test.js
@@ -1,16 +1,19 @@
 const shipFile = require('./ship');
 const { TestWatcher } = require("jest");
-const mockShip = shipFile(coord1 = {x: 1, y: 1}, coord2 = {x: 1, y: 1});
+const mockShip = shipFile(coord1 = {x: 1, y: 1}, coord2 = {x: 1, y: 7});
 console.log(mockShip);
 test("mockShip returns an object", () => {
-    expect(mockShip).toBe({ coord1: [1, 1], coord2: [1, 7]});
+    expect(mockShip).toBe({ coord1: [1, 1], coord2: [1, 7], lengthMeasurer, hit });
 })
 test("mockShip returns a length of 6", () => {
     expect(mockShip.lengthMeasurer(coord1 = {x: 1, y: 1}, coord2 = {x: 1, y: 7})).toBe(6);
 })
-test.only("mockShip will get hit at [1, 1]", () => {
+test("mockShip will get hit at [1, 1]", () => {
     expect(mockShip.hit({x: 1, y: 1})).toBe(true);
 })
-test("mockShip will not sink", () => {
+test("mockShip will get hit at [1, 7]", () => {
+    expect(mockShip.hit({x: 1, y: 7})).toBe(true);
+})
+test.skip("mockShip will not sink", () => {
     expect(shipFile.shipFactory.sink().toBe(false));
 })
