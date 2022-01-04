077572eeff9b83f78a49847fa540f6993d5caa48
jackalotUVM <gonzalezc9999@gmail.com>
2022-01-04 08:38:00 -0500

    replaced shipFile with mockShip

--- src/index.test.js
+++ src/index.test.js
@@ -6,7 +6,7 @@ test("mockShip returns an object", () => {
     expect(mockShip).toStrictEqual({ coord1: [1, 1], coord2: [1, 7]});
 })
 test.only("ship factory return a length of 6", () => {
-    expect(shipFile[0].lengthMeasurer(coord1 = {x: 1, y: 1}, coord2 = {x: 1, y: 7})).toBe(6);
+    expect(mockShip.lengthMeasurer(coord1 = {x: 1, y: 1}, coord2 = {x: 1, y: 7})).toBe(6);
 })
 test("ship factory will get hit at [1, 1]", () => {
     expect(shipFile.shipFactory.hit([1, 1], { coord1: [1, 1], coord2: [1, 7]})).toBe(true);
