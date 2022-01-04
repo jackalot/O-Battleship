81fb2648be41907730086ad9a4dfdaf3cf92eb40
jackalotUVM <gonzalezc9999@gmail.com>
2022-01-04 08:37:30 -0500

    moved the only method to the second test

--- src/index.test.js
+++ src/index.test.js
@@ -2,10 +2,10 @@ const shipFile = require('./ship');
 const { TestWatcher } = require("jest");
 const mockShip = shipFile(coord1 = [1, 1], coord2 = [1, 7]);
 console.log(mockShip);
-test.only("mockShip returns an object", () => {
+test("mockShip returns an object", () => {
     expect(mockShip).toStrictEqual({ coord1: [1, 1], coord2: [1, 7]});
 })
-test("ship factory return a length of 6", () => {
+test.only("ship factory return a length of 6", () => {
     expect(shipFile[0].lengthMeasurer(coord1 = {x: 1, y: 1}, coord2 = {x: 1, y: 7})).toBe(6);
 })
 test("ship factory will get hit at [1, 1]", () => {
