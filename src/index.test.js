95a7af343a4b7625fb0da60a0483196383eb54cd
jackalotUVM <gonzalezc9999@gmail.com>
2022-01-04 08:36:58 -0500

    initialized a mock of the shipFactory function called mock ship, changed the first test to not call a function and call the new mock

--- src/index.test.js
+++ src/index.test.js
@@ -1,11 +1,12 @@
 const shipFile = require('./ship');
 const { TestWatcher } = require("jest");
-console.log(shipFile.lengthMeasurer);
-test("ship factory returns an object", () => {
-    expect(shipFile( coord1 = [1, 1], coord2 = [1, 7])).toStrictEqual({ coord1: [1, 1], coord2: [1, 7]});
+const mockShip = shipFile(coord1 = [1, 1], coord2 = [1, 7]);
+console.log(mockShip);
+test.only("mockShip returns an object", () => {
+    expect(mockShip).toStrictEqual({ coord1: [1, 1], coord2: [1, 7]});
 })
-test.only("ship factory return a length of 6", () => {
-    expect(shipFile[0].lengthMeasurer( coord1 = {x: 1, y: 1}, coord2 = {x: 1, y: 7})).toBe(6);
+test("ship factory return a length of 6", () => {
+    expect(shipFile[0].lengthMeasurer(coord1 = {x: 1, y: 1}, coord2 = {x: 1, y: 7})).toBe(6);
 })
 test("ship factory will get hit at [1, 1]", () => {
     expect(shipFile.shipFactory.hit([1, 1], { coord1: [1, 1], coord2: [1, 7]})).toBe(true);
