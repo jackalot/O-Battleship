84e09150d290123b50481fd39cbb4358c5b17fe8
jackalotUVM <gonzalezc9999@gmail.com>
2022-01-05 09:40:07 -0500

    removed the only method

--- src/index.test.js
+++ src/index.test.js
@@ -18,6 +18,6 @@ test("mockShip will get hit at [1, 7]", () => {
 test("mockShip.shipParts will return every part", () => {
     expect(mockShip.shipParts()).toEqual([{"hasSunk": false, "partCoordinates": {"x": 1, "y": 1}, "partName": "part1"}, {"hasSunk": false, "partCoordinates": {"x": 2, "y": 1}, "partName": "part2"}, {"hasSunk": false, "partCoordinates": {"x": 3, "y": 1}, "partName": "part3"}, {"hasSunk": false, "partCoordinates": {"x": 4, "y": 1}, "partName": "part4"}, {"hasSunk": false, "partCoordinates": {"x": 5, "y": 1}, "partName": "part5"}]    );
 })
-test.only("mockShip will not sink", () => {
+test("mockShip will not sink", () => {
     expect(mockShip.sink()).toBe('I still have parts left');
 })
