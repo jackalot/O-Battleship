a1b780ffe610af070f3202e9c2c357d2e1c222b8
jackalotUVM <gonzalezc9999@gmail.com>
2022-01-06 09:12:58 -0500

    replaced sample test with a possible first test for the boardFile

--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,7 @@
 const { TestWatcher } = require("jest");
 const boardFile = require('./board');
-test("this is a sample test so this suite passes", () => {
-    expect(2 + 2).toBe(4);
+const shipFile = require('./ship');
+test("gameBoard has placed a ship from [1,1] to [1,1]", () => {
+    //so if game board has a function called placeShip, and it places a ship that will have a return function, it will return its coordinates?
+    expect(boardFile.placeShip(coord1 = {x: 1, y: 1}, coord2 = {x: 1, y: 7})).toBe(coord1 = {x: 1, y: 1}, coord2 = {x: 1, y: 7});
 })
