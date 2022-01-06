8af6b4fcef617b4c40b78534d88b32cf3f575a3c
jackalotUVM <gonzalezc9999@gmail.com>
2022-01-06 10:17:39 -0500

    imported ship factory, added paramaters, added some functions, added some return values to gameBoardFactory

--- src/board.js
+++ src/board.js
@@ -1,4 +1,13 @@
-const gameBoardFactory = () {
+import shipFactory from './ship';
 
-}
+const gameBoardFactory = (bottomLeftCorner, topRightCorner) => {
+    //This checks if its not out of bounds
+  function checkValidCoord() {
+    if
+  }
+  function placeShip(firstCoord, secondCoord) {
+
+  }
+  return { bottomLeftCorner, topRightCorner, placeShip };
+};
 module.exports = gameBoardFactory;
