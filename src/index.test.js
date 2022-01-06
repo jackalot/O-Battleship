af9b41b3912d715323c842501b0ae6ec9054a41f
jackalotUVM <gonzalezc9999@gmail.com>
2022-01-06 10:19:50 -0500

    created a mockup of the board

--- src/index.test.js
+++ src/index.test.js
@@ -1,6 +1,7 @@
 const { TestWatcher } = require("jest");
 const boardFile = require('./board');
 const shipFile = require('./ship');
+const mockBoard = boardFile(coord1 = {x: 1, y: 1}, coord2 = {x: 10, y: 10})
 test("gameBoard has placed a ship from [1,1] to [1,7]", () => {
     //so if game board has a function called placeShip, and it places a ship that will have a return function, it will return its coordinates?
     expect(boardFile.placeShip(coord1 = {x: 1, y: 1}, coord2 = {x: 1, y: 7})).toBe(coord1 = {x: 1, y: 1}, coord2 = {x: 1, y: 7});
