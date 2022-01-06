e132dbeb932f16bd90f268181de88762c5741279
jackalotUVM <gonzalezc9999@gmail.com>
2022-01-06 09:24:02 -0500

    fixed a typo, added two tests for recieving an attack

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,15 @@
 const { TestWatcher } = require("jest");
 const boardFile = require('./board');
 const shipFile = require('./ship');
-test("gameBoard has placed a ship from [1,1] to [1,1]", () => {
+test("gameBoard has placed a ship from [1,1] to [1,7]", () => {
     //so if game board has a function called placeShip, and it places a ship that will have a return function, it will return its coordinates?
     expect(boardFile.placeShip(coord1 = {x: 1, y: 1}, coord2 = {x: 1, y: 7})).toBe(coord1 = {x: 1, y: 1}, coord2 = {x: 1, y: 7});
+})
+//it also sends the hit() function to the corresponding ship
+test("gameBoard recieves an attack at [1, 1] and it hits due to the ship there", () => {
+    expect(boardFile.recieveAttack(coord1 = {x: 1, y: 1}, coord2 = {x: 1, y: 7})).toBe(true);
+})
+//it also keeps track of the missed attacks
+test("gameBoard recieves an attack at [2, 6] and it misses due to no ship there", () => {
+    expect(boardFile.recieveAttack(coord1 = {x: 1, y: 1}, coord2 = {x: 1, y: 7})).toBe(true);
 })
