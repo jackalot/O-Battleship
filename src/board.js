38d1ad7f4ac0e1a42f30cf3541a1affcdce89bce
jackalotUVM <gonzalezc9999@gmail.com>
2022-01-06 10:36:29 -0500

    added checkBottomLeftCorner and checkTopRightCorner helperFunctions for checkValidCoord

--- src/board.js
+++ src/board.js
@@ -1,22 +1,34 @@
 import shipFactory from './ship';
 
 const gameBoardFactory = (bottomLeftCorner, topRightCorner) => {
-  //  placeShip() helper function
-  // This checks if its not out of bounds
-  function checkValidCoord(coord) {
+  //  checkValidCorner() hellper function
+  // checks if the coord is out of bounds of the bottomLeftCorner
+  function checkBottomLeftCorner(coord) {
     if (coord.x >= bottomLeftCorner.x) {
       if (coord.y >= bottomLeftCorner.y) {
-        if (coord.y <= topRightCorner.x) {
-          if (coord.y <= topRightCorner) {
-            return true;
-          }
-          return false;
-        }
         return false;
+      }
+      return true;
+    }
+    return true;
+  }
+  function checkTopRightCorner(coord) {
+    if (coord.x <= topRightCorner.x) {
+      if (coord.y <= topRightCorner.y) {
+        return true;
       }
       return false;
     }
     return false;
+  }
+  //  placeShip() helper function
+  // This checks if its not out of bounds
+  function checkValidCoord(coord) {
+    const CBLC = checkBottomLeftCorner(coord);
+    const CTPRC = checkTopRightCorner(coord);
+    if (CBLC === true && CTPRC === true) {
+      return true;
+    }
   }
   function placeShip(firstCoord, secondCoord) {
     const checkFirstCoord = checkValidCoord(checkValidCoord);
