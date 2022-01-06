7bd717dfc33b901d4c070a5223969da32970bfcd
jackalotUVM <gonzalezc9999@gmail.com>
2022-01-06 11:25:42 -0500

    checkValidCOord now has its own test

--- src/index.test.js
+++ src/index.test.js
@@ -2,7 +2,10 @@ const { TestWatcher } = require("jest");
 const boardFile = require('./board');
 const shipFile = require('./ship');
 const mockBoard = boardFile(coord1 = {x: 1, y: 1}, coord2 = {x: 10, y: 10})
-test.only("gameBoard has placed a ship from [1,1] to [1,7]", () => {
+test.only("[1,1] is a valid coordinate on tghe game board", () => {
+    expect(mockboard.checkValidCoord({x: 1, y: 1})).toBe(true);
+})
+test("gameBoard has placed a ship from [1,1] to [1,7]", () => {
     //so if game board has a function called placeShip, and it places a ship that will have a return function, it will return its coordinates?
     expect(mockBoard.placeShip(coord1 = {x: 1, y: 1}, coord2 = {x: 1, y: 7})).toBe(coord1 = {x: 1, y: 1}, coord2 = {x: 1, y: 7});
 })
