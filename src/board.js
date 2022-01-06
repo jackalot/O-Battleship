fa560be02ed044159bbd9ecb17562a9907738735
jackalotUVM <gonzalezc9999@gmail.com>
2022-01-06 10:26:33 -0500

    added helperfunction 'checkValidCoord' for 'placeShip

--- src/board.js
+++ src/board.js
@@ -1,12 +1,25 @@
 import shipFactory from './ship';
 
 const gameBoardFactory = (bottomLeftCorner, topRightCorner) => {
-    //This checks if its not out of bounds
-  function checkValidCoord() {
-    if
+  //  placeShip() helper function
+  // This checks if its not out of bounds
+  function checkValidCoord(coord) {
+    if (coord.x >= bottomLeftCorner.x) {
+      if (coord.y >= bottomLeftCorner.y) {
+        if (coord.y <= topRightCorner.x) {
+          if (coord.y <= topRightCorner) {
+            return true;
+          }
+          return false;
+        }
+        return false;
+      }
+      return false;
+    }
+    return false;
   }
   function placeShip(firstCoord, secondCoord) {
-
+    const checkFirstCoord = checkValidCoord(checkValidCoord);
   }
   return { bottomLeftCorner, topRightCorner, placeShip };
 };
