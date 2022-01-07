5e5b057636aabb0f04071ba3c7296700d16e510f
jackalotUVM <gonzalezc9999@gmail.com>
2022-01-07 10:42:05 -0500

    added tests for the corners as a whole, added skip methods to the tests we havent wrote scripts for yet

--- src/index.test.js
+++ src/index.test.js
@@ -4,13 +4,13 @@ const shipFile = require('./ship');
 const mockBoard = boardFile(coord1 = {x: 1, y: 1}, coord2 = {x: 10, y: 10})
 // checkValidCoord() helper functions marked with ![
 // checkBottomLeftCorner() helper functions marked with ?[
-test.skip("[1,1] is equal to the bottom left corner, [1, 1]", () => {
+test("[1,1] is equal to the bottom left corner, [1, 1]", () => {
     expect(mockBoard.checkEqualToBLC({x: 1, y: 1})).toBe(true);
 })
-test.skip("[1,7] is equal to the bottom left corner, [1, 1]", () => {
+test("[1,7] is equal to the bottom left corner, [1, 1]", () => {
     expect(mockBoard.checkEqualToBLC({x: 1, y: 7})).toBe(false);
 })
-test.skip("[1,1] is not smaller than the bottom left corner, [1, 1]", () => {
+test("[1,1] is not smaller than the bottom left corner, [1, 1]", () => {
     expect(mockBoard.checkGreaterThanBLC({x: 1, y: 1})).toBe(false);
 })
 test.skip("[1,7] is not smaller than the bottom left corner, [1, 1]", () => {
@@ -23,31 +23,43 @@ test("[1,1] is not equal to the top right corner, [10, 10]", () => {
 test("[1,7] is not equal to the top right corner, [10, 10]", () => {
     expect(mockBoard.checkEqualToTRC({x:1, y: 7})).toBe(false);
 })
-test.only("[1,1] is not greater than the top right corner, [10, 10]", () => {
+test("[1,1] is not greater than the top right corner, [10, 10]", () => {
     expect(mockBoard.checkLessThanTRC({x:1, y: 1})).toBe(true);
 })
-test.only("[1,7] is not greater than the top right corner, [10, 10]", () => {
+test("[1,7] is not greater than the top right corner, [10, 10]", () => {
     expect(mockBoard.checkLessThanTRC({x:1, y: 7})).toBe(true);
 })
 // ]!
+test("[1,1] is a valid according to the Bottom Left Corner", () => {
+    expect(mockBoard.checkBottomLeftCorner({x: 1, y: 1})).toBe(true);
+})
+test("[1,7] is a valid according to the Bottom Left Corner", () => {
+    expect(mockBoard.checkBottomLeftCorner({x: 1, y: 7})).toBe(true);
+})
+test("[1,1] is a valid according to the Top Right Corner", () => {
+    expect(mockBoard.checkTopRightCorner({x: 1, y: 1})).toBe(true);
+})
+test("[1,7] is a valid according to the Top Right Corner", () => {
+    expect(mockBoard.checkTopRightCorner({x: 1, y: 7})).toBe(true);
+})
 test("[1,1] is a valid coordinate on the game board", () => {
     expect(mockBoard.checkValidCoord({x: 1, y: 1})).toBe(true);
 })
 test("[1,7] is a valid coordinate on the game board", () => {
     expect(mockBoard.checkValidCoord({x: 1, y: 7})).toBe(true);
 })
-test("gameBoard has placed a ship from [1,1] to [1,7]", () => {
+test.skip("gameBoard has placed a ship from [1,1] to [1,7]", () => {
     //so if game board has a function called placeShip, and it places a ship that will have a return function, it will return its coordinates?
     expect(mockBoard.placeShip(coord1 = {x: 1, y: 1}, coord2 = {x: 1, y: 7})).toBe(coord1 = {x: 1, y: 1}, coord2 = {x: 1, y: 7});
 })
 //it also sends the hit() function to the corresponding ship
-test("gameBoard recieves an attack at [1, 1] and it hits due to the ship there", () => {
+test.skip("gameBoard recieves an attack at [1, 1] and it hits due to the ship there", () => {
     expect(boardFile.recieveAttack(coord1 = {x: 1, y: 1}, coord2 = {x: 1, y: 7})).toBe(true);
 })
 //it also keeps track of the missed attacks
-test("gameBoard recieves an attack at [2, 6] and it misses due to no ship there", () => {
+test.skip("gameBoard recieves an attack at [2, 6] and it misses due to no ship there", () => {
     expect(boardFile.recieveAttack(coord1 = {x: 1, y: 1}, coord2 = {x: 1, y: 7})).toBe(true);
 })
-test("gameBoard checks if all ships have sunk, and returns false as theres still one ship up", () => {
-    expec(boardFile.allShipsSunk()).toBe(false);
+test.skip("gameBoard checks if all ships have sunk, and returns false as theres still one ship up", () => {
+    expect(boardFile.allShipsSunk()).toBe(false);
 })
