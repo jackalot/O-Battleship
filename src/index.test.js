a5a7574a56708fe8b54ced72927d37e4b513ced5
jackalotUVM <gonzalezc9999@gmail.com>
2022-01-07 09:29:23 -0500

    added a only method to the check valid coord test, and added a new test for check valid coord

--- src/index.test.js
+++ src/index.test.js
@@ -18,8 +18,11 @@ test.skip("[1,7] is not smaller than the bottom left corner, [1, 1]", () => {
 })
 // ]?
 // ]!
-test("[1,1] is a valid coordinate on the game board", () => {
+test.only("[1,1] is a valid coordinate on the game board", () => {
     expect(mockBoard.checkValidCoord({x: 1, y: 1})).toBe(true);
+})
+test.only("[1,1] is a valid coordinate on the game board", () => {
+    expect(mockBoard.checkValidCoord({x: 1, y: 7})).toBe(true);
 })
 test("gameBoard has placed a ship from [1,1] to [1,7]", () => {
     //so if game board has a function called placeShip, and it places a ship that will have a return function, it will return its coordinates?
