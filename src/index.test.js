09ef0d5195018f4f1c3c82a354dfc3d15f449517
jackalotUVM <gonzalezc9999@gmail.com>
2022-01-12 10:41:44 -0500

    removed skip method on recieveAttack test

--- src/index.test.js
+++ src/index.test.js
@@ -53,7 +53,7 @@ test("gameBoard has placed a ship from [1,1] to [1,7]", () => {
     expect(mockBoard.placeShip( {x: 1, y: 1}, {x: 1, y: 7})).toMatchObject({ coord1: {x: 1, y: 1}, coord2: {x: 1, y: 7} });
 })
 //it also sends the hit() function to the corresponding ship
-test.skip("gameBoard recieves an attack at [1, 1] and it hits due to the ship there", () => {
+test("gameBoard recieves an attack at [1, 1] and it hits due to the ship there", () => {
     expect(boardFile.recieveAttack(coord1 = {x: 1, y: 1}, coord2 = {x: 1, y: 7})).toBe(true);
 })
 //it also keeps track of the missed attacks
