c64f86e77ec60889ca6304239ff238c15378fdb1
jackalotUVM <gonzalezc9999@gmail.com>
2022-01-12 10:44:21 -0500

    replaced boardFile with mockBoard in recieveAttack test, eliminated a space in placeShip test

--- src/index.test.js
+++ src/index.test.js
@@ -50,11 +50,11 @@ test("[1,7] is a valid coordinate on the game board", () => {
 })
 test("gameBoard has placed a ship from [1,1] to [1,7]", () => {
     //so if game board has a function called placeShip, and it places a ship that will have a return function, it will return its coordinates?
-    expect(mockBoard.placeShip( {x: 1, y: 1}, {x: 1, y: 7})).toMatchObject({ coord1: {x: 1, y: 1}, coord2: {x: 1, y: 7} });
+    expect(mockBoard.placeShip({x: 1, y: 1}, {x: 1, y: 7})).toMatchObject({ coord1: {x: 1, y: 1}, coord2: {x: 1, y: 7} });
 })
 //it also sends the hit() function to the corresponding ship
 test("gameBoard recieves an attack at [1, 1] and it hits due to the ship there", () => {
-    expect(boardFile.recieveAttack(coord1 = {x: 1, y: 1}, coord2 = {x: 1, y: 7})).toBe(true);
+    expect(mockBoard.recieveAttack({x: 1, y: 1})).toBe(true);
 })
 //it also keeps track of the missed attacks
 test.skip("gameBoard recieves an attack at [2, 6] and it misses due to no ship there", () => {
