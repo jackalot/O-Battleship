fc3644092de5325d76a3c731cec7e6a50caddcfe
jackalotUVM <gonzalezc9999@gmail.com>
2022-01-13 10:26:01 -0500

    created two mockPlayers, player1, player2

--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,7 @@
 const { TestWatcher } = require("jest");
 const playerFile = require("player.js");
+const player1 = playerFile("Player 1", 0);
+const player2 = playerFile("Player 2", 1);
 test("sample test", () => {
     expect(2 + 2).toBe(4);
 })
