3059cad60836c34541d2902e84ee38efac5d51e4
jackalotUVM <gonzalezc9999@gmail.com>
2022-01-14 09:58:48 -0500

    added function swapTurn which will swap the players turns

--- src/player.js
+++ src/player.js
@@ -2,6 +2,19 @@ const boardFile = require('./board');
 
 const playerFactory = (playerName, playerTurn) => {
   const ourBoard = boardFile({ x: 1, y: 1 }, { x: 10, y: 10 });
+  function swapTurn() {
+    // eslint doesnt like this function but its the easiest
+    //  solution other than true or false
+    if (playerTurn === 0) {
+      playerTurn++;
+      return playerTurn;
+    }
+    if (playerTurn === 1) {
+      playerTurn--;
+      return playerTurn;
+    }
+    return null;
+  }
   function sendAttack(playerAttacking, coordinates) {
     const result = playerAttacking.ourBoard.recieveAttack(coordinates);
     return result;
@@ -16,7 +29,7 @@ const playerFactory = (playerName, playerTurn) => {
   }
   sayHi();
   return {
-    playerName, playerTurn, ourBoard, sendAttack, placeShip,
+    playerName, playerTurn, ourBoard, sendAttack, placeShip, swapTurn,
   };
 };
 module.exports = playerFactory;
