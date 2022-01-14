5f400b4d4ae5e2d0de208888439f9ff65781cfab
jackalotUVM <gonzalezc9999@gmail.com>
2022-01-14 10:02:17 -0500

    replaced the second swapTurn test having player2 when it should have had player 1, also gave the same test an only method

--- src/index.test.js
+++ src/index.test.js
@@ -12,11 +12,11 @@ test("player1 sends an attack to player2's board at [2,2] and it hits", () => {
     expect(player1.sendAttack(player2, {x: 2, y: 2})).toBe(true);
 })
 //swapTurn tests will break if used with other tests, skip otherwise
-test.only("player2 will now have the player turn 0 (meaning its their turn)", () => {
+test.skip("player2 will now have the player turn 0 (meaning its their turn)", () => {
     expect(player2.swapTurn()).toBe(0);
 })
-test.skip("player1 will now have the player turn 1 (meaning its not their turn)", () => {
-    expect(player2.swapTurn()).toBe(1);
+test.only("player1 will now have the player turn 1 (meaning its not their turn)", () => {
+    expect(player1.swapTurn()).toBe(1);
 })
 // true in this case means that after sending random attacks it managed to attack a valid coordinate
 test.skip("player2(CPU) sends a random attack to player1's board and it will return true", ()=> {
