a634f7cb60ec61532f7111bd85e43c422d069573
jackalotUVM <gonzalezc9999@gmail.com>
2022-01-14 08:26:34 -0500

    added a test for 'sendRandomAttack', a function for the computer to send a random attack on player1s board

--- src/index.test.js
+++ src/index.test.js
@@ -5,3 +5,7 @@ const player2 = playerFile("Player 2", 1);
 test("player1 sends an attack to player2's board at [2,2] and it hits", () => {
     expect(player1.sendAttack(player2, {x: 2, y: 2})).toBe(true);
 })
+// true in this case means that after sending random attacks it managed to attack a valid coordinate
+test.skip("player2(CPU) sends a random attack to player1's board and it will return true", ()=> {
+expect(player2.sendRandomAttack(player1)).toBe(true);
+})
