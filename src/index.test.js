386c969aa5e9a16ca4c1d16cdd345dbf13d6876a
jackalotUVM <gonzalezc9999@gmail.com>
2022-01-14 09:18:13 -0500

    added a second test for placeShip where player 2 places a ship

--- src/index.test.js
+++ src/index.test.js
@@ -5,6 +5,9 @@ const player2 = playerFile("Player 2", 1);
 test.only("player1 places a ship from [1, 1] to [1,7]", () => {
     expect(player1.placeShip( {x: 1, y: 1}, {x: 1, y: 7})).toMatchObject({ coord1: {x: 1, y: 1}, coord2: {x: 1, y: 7} });
 })
+test.only("player2 places a ship from [2, 2] to [2, 6]", () => {
+    expect(player2.placeShip( {x: 2, y: 2}, {x: 2, y: 6})).toMatchObject({ coord1: {x: 2, y: 2}, coord2: {x: 2, y: 6} });
+})
 test.only("player1 sends an attack to player2's board at [2,2] and it hits", () => {
     expect(player1.sendAttack(player2, {x: 2, y: 2})).toBe(true);
 })
