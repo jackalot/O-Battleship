851cc8ff3554f60c2fbaefd50a816be1628f437d
jackalotUVM <gonzalezc9999@gmail.com>
2022-01-14 08:18:33 -0500

    wrote a test for a function called 'sendAttack' which would send an attack to another players board

--- src/index.test.js
+++ src/index.test.js
@@ -2,6 +2,6 @@ const { TestWatcher } = require("jest");
 const playerFile = require("./player");
 const player1 = playerFile("Player 1", 0);
 const player2 = playerFile("Player 2", 1);
-test("sample test", () => {
-    expect(2 + 2).toBe(4);
+test("player1 sends an attack to player2's board at [2,2] and it hits", () => {
+    expect(player1.sendAttack(player2, {x: 2, y: 2})).toBe(true);
 })
