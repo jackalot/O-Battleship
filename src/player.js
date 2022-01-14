c1ba9612ac44218399e3df353297b3aed0fbe6e2
jackalotUVM <gonzalezc9999@gmail.com>
2022-01-14 08:33:08 -0500

    imported bthe boardFile via require(), in the player factory we now have our own board, and we now return it

--- src/player.js
+++ src/player.js
@@ -1,10 +1,13 @@
+const boardFile = require('./board');
+
 const playerFactory = (playerName, playerTurn) => {
+  const ourBoard = boardFile({ x: 1, y: 1 }, { x: 10, y: 10 });
   function sayHi() {
     console.log('hi');
   }
   sayHi();
   return {
-    playerName, playerTurn,
+    playerName, playerTurn, ourBoard,
   };
 };
 module.exports = playerFactory;
