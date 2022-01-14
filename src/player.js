40eea5ac98baabb7bf7a2d9c3a8d753d7354dec2
jackalotUVM <gonzalezc9999@gmail.com>
2022-01-14 08:59:22 -0500

    created function 'sendAttack' which gets the player we are attacking and the coordinates, and sends an attack to that players board and calls 'recieveAttack' on their board.

--- src/player.js
+++ src/player.js
@@ -2,12 +2,16 @@ const boardFile = require('./board');
 
 const playerFactory = (playerName, playerTurn) => {
   const ourBoard = boardFile({ x: 1, y: 1 }, { x: 10, y: 10 });
+  function sendAttack(playerAttacking, coordinates) {
+    const result = playerAttacking.ourBoard.recieveAttack(coordinates);
+    return result;
+  }
   function sayHi() {
     console.log('hi');
   }
   sayHi();
   return {
-    playerName, playerTurn, ourBoard,
+    playerName, playerTurn, ourBoard, sendAttack,
   };
 };
 module.exports = playerFactory;
