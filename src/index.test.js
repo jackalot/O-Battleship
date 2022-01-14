0c8bf6782208863920c916652cd5241fa450b066
jackalotUVM <gonzalezc9999@gmail.com>
2022-01-14 08:32:15 -0500

    the player 1 send attack test has an only method now

--- src/index.test.js
+++ src/index.test.js
@@ -2,7 +2,7 @@ const { TestWatcher } = require("jest");
 const playerFile = require("./player");
 const player1 = playerFile("Player 1", 0);
 const player2 = playerFile("Player 2", 1);
-test("player1 sends an attack to player2's board at [2,2] and it hits", () => {
+test.only("player1 sends an attack to player2's board at [2,2] and it hits", () => {
     expect(player1.sendAttack(player2, {x: 2, y: 2})).toBe(true);
 })
 // true in this case means that after sending random attacks it managed to attack a valid coordinate
