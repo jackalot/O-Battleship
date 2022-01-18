d7d6d02997cff60fd01f7079a3321423316a4672
jackalotUVM <gonzalezc9999@gmail.com>
2022-01-18 10:25:40 -0500

    querySelected both of our boards and imported the playerFile, also instantiiated two players from playerFile

---
+++ src/index.js
@@ -0,0 +1,6 @@
+const playerOneDomBoard = document.querySelector('#playerOneBoard');
+const playerTwoDomBoard = document.querySelector('#playerTwoBoard');
+const playerFile = require('./player');
+
+const playerOne = playerFile('player 1', 0);
+const playerTwo = playerFile('CPU', 1);
