e5e75139788628177cb3e4816b3182c85b512415
jackalotUVM <gonzalezc9999@gmail.com>
2022-01-19 08:28:16 -0500

    added two declarations in index.js, two ships have been placed

--- src/index.js
+++ src/index.js
@@ -4,3 +4,5 @@ const playerFile = require('./player');
 
 const playerOne = playerFile('player 1', 0);
 const playerTwo = playerFile('CPU', 1);
+playerOne.ourBoard.placeShip({ x: 1, y: 1 }, { x: 1, y: 7 });
+playerTwo.ourBoard.placeShip({ x: 2, y: 2 }, { x: 8, y: 2 });
