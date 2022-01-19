2ff2c2e48458b9a8d715b4421377924ac5028d4a
jackalotUVM <gonzalezc9999@gmail.com>
2022-01-19 08:36:38 -0500

    added function populateBoard

--- src/index.js
+++ src/index.js
@@ -6,3 +6,10 @@ const playerOne = playerFile('player 1', 0);
 const playerTwo = playerFile('CPU', 1);
 playerOne.ourBoard.placeShip({ x: 1, y: 1 }, { x: 1, y: 7 });
 playerTwo.ourBoard.placeShip({ x: 2, y: 2 }, { x: 8, y: 2 });
+function populateBoard(board, player) {
+    for(let i = 0; i < player.ourBoard.topRightCorner.x; i += 1)
+    {
+        const square = document.createElement("p");
+    }
+board.append(player.ourBoard.)
+}
