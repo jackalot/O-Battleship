4b4d526ba805e5124e3e65cadbae8bd98f9263f3
jackalotUVM <gonzalezc9999@gmail.com>
2022-01-27 09:03:18 -0500

    placed a ship for playerTwo

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const playerFile = require('./player');
 const playerOne = playerFile('player 1', 0);
 const playerTwo = playerFile('CPU', 1);
 playerOne.ourBoard.placeShip({ x: 1, y: 1 }, { x: 1, y: 7 });
+playerTwo.ourBoard.placeShip({ x: 2, y: 2 }, { x: 7, y: 2 });
 console.log(playerOne.ourBoard.topRightCorner);
 console.log(playerOne.ourBoard.bottomLeftCorner);
 console.log(playerTwo.ourBoard.topRightCorner);
