5c30db9e3598aa81c31215fa6fd6d09848a63a76
jackalotUVM <gonzalezc9999@gmail.com>
2022-02-01 09:15:35 -0500

    changed the y levels in the queryString and coordinates for the third row 3, changed the math.floor operation to divide by 20

--- src/index.js
+++ src/index.js
@@ -71,11 +71,11 @@ function populateBoard(board, player) {
       }
     }
     if (i < 31 && i > 20) {
-      const xAxis = Math.floor(i / 10);
+      const xAxis = Math.floor(i / 20);
       if (player === playerOne) {
-        createBox(`#P1Box-${xAxis}-2`, player, { x: xAxis, y: 2 });
+        createBox(`#P1Box-${xAxis}-3`, player, { x: xAxis, y: 3 });
       } else if (player === playerTwo) {
-        createBox(`#P2Box-${xAxis}-2`, player, { x: xAxis, y: 2 });
+        createBox(`#P2Box-${xAxis}-3`, player, { x: xAxis, y: 3 });
       }
     }
   }
