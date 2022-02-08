693cbdd98038e9894ffa2446937b5839b639a2ae
jackalotUVM <gonzalezc9999@gmail.com>
2022-02-08 08:45:39 -0500

    index.js now requires updateDomBoard, and has a player1DomBoardManager which will manage the board of player 1 each time.

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import './style.css';
 const playerOneDomBoard = document.querySelector('#playerOneBoard');
 const playerTwoDomBoard = document.querySelector('#playerTwoBoard');
 const playerFile = require('./player');
+const domBoardFile = require('./updateDOMBoard');
 
 const playerOne = playerFile('player 1', 0);
 const playerTwo = playerFile('CPU', 1);
+const p1DomBoardManager = domBoardFile(playerOne, '#P1Box');
 playerOne.ourBoard.placeShip({ x: 1, y: 1 }, { x: 1, y: 7 });
 playerTwo.ourBoard.placeShip({ x: 1, y: 1 }, { x: 1, y: 7 });
 playerOne.ourBoard.placeShip({ x: 2, y: 1 }, { x: 8, y: 1 });
