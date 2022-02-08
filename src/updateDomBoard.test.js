1fca39e01a6ee9123cca8407de98572749c2a384
jackalotUVM <gonzalezc9999@gmail.com>
2022-02-08 10:17:15 -0500

    imported jest-dom testing library

--- src/updateDomBoard.test.js
+++ src/updateDomBoard.test.js
@@ -2,3 +2,4 @@ const updateDOMBoardFile = require("./updateDOMBoard.js");
 const playerFile = require("./player");
 const player1 = playerFile("Player 1", 0);
 const p1DomBoardManager = updateDOMBoardFile(player1, 'P1Box');
+import '@testing-library/jest-dom';
