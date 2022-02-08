3e744e7b505528d5b20f8d1e3122d70e5ffd89a4
jackalotUVM <gonzalezc9999@gmail.com>
2022-02-08 10:28:35 -0500

    in the updateDomBoard.test.js, player1 now places a ship

--- src/updateDomBoard.test.js
+++ src/updateDomBoard.test.js
@@ -3,3 +3,4 @@ const playerFile = require("./player");
 const player1 = playerFile("Player 1", 0);
 const p1DomBoardManager = updateDOMBoardFile(player1, 'P1Box');
 import '@testing-library/jest-dom';
+player1.placeShip({ x: 1, y: 1 });
