18fb738dd26d7a662ad88707c45b2aaf7333435c
jackalotUVM <gonzalezc9999@gmail.com>
2022-02-08 10:33:03 -0500

    added a temp box

--- src/updateDomBoard.test.js
+++ src/updateDomBoard.test.js
@@ -5,3 +5,5 @@ const p1DomBoardManager = updateDOMBoardFile(player1, 'P1Box');
 import '@testing-library/jest-dom';
 player1.placeShip({ x: 1, y: 1 }, { x: 1, y: 2 });
 p1DomBoardManager.sendNextCollumn();
+const tempBox = getByTestId(`P1Box-1-1`)
+expect()
