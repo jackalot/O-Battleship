da5c0d3d9b5922d6f5c5c55c32c466039c88241d
jackalotUVM <gonzalezc9999@gmail.com>
2022-02-08 10:30:49 -0500

    p1DomBoardManager now sends a collumn after player 1 places a ship, when player 1 places a ship, it now has two sets oof coords

--- src/updateDomBoard.test.js
+++ src/updateDomBoard.test.js
@@ -3,4 +3,5 @@ const playerFile = require("./player");
 const player1 = playerFile("Player 1", 0);
 const p1DomBoardManager = updateDOMBoardFile(player1, 'P1Box');
 import '@testing-library/jest-dom';
-player1.placeShip({ x: 1, y: 1 });
+player1.placeShip({ x: 1, y: 1 }, { x: 1, y: 2 });
+p1DomBoardManager.sendNextCollumn();
