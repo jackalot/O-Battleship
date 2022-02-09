b34529043b0bd5704201e4cc0b89c1c2c1c0566c
jackalotUVM <gonzalezc9999@gmail.com>
2022-02-09 08:25:08 -0500

    added a test for temp box in updateDomBoard.test.js

--- src/updateDomBoard.test.js
+++ src/updateDomBoard.test.js
@@ -4,4 +4,7 @@ const player1 = playerFile("Player 1", 0);
 const p1DomBoardManager = updateDOMBoardFile(player1, 'P1Box');
 player1.placeShip({ x: 1, y: 1 }, { x: 1, y: 2 });
 p1DomBoardManager.sendNextCollumn();
-const tempBox = getByTestId(`P1Box-1-1`)
+const tempBox = getByTestId(`P1Box-1-1`);
+test("temp box with the id of 'P1Box-1-1' has the classname of hit", () => {
+    expect(p1DomBoardManager.checkForClass(tempBox, "hit")).toBe(true);
+})
