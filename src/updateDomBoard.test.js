8de39cc2f410a9e670e2c4699e32bc3635400d3b
jackalotUVM <gonzalezc9999@gmail.com>
2022-02-09 08:19:06 -0500

    removed references to the custom library

--- src/updateDomBoard.test.js
+++ src/updateDomBoard.test.js
@@ -2,8 +2,6 @@ const updateDOMBoardFile = require("./updateDOMBoard.js");
 const playerFile = require("./player");
 const player1 = playerFile("Player 1", 0);
 const p1DomBoardManager = updateDOMBoardFile(player1, 'P1Box');
-import '@testing-library/jest-dom';
 player1.placeShip({ x: 1, y: 1 }, { x: 1, y: 2 });
 p1DomBoardManager.sendNextCollumn();
 const tempBox = getByTestId(`P1Box-1-1`)
-expect(tempBox).toHaveClass(containsShip);
