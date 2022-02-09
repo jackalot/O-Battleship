39e9e124609186681e35a0d272eb052811233f7b
jackalotUVM <gonzalezc9999@gmail.com>
2022-02-09 08:29:43 -0500

    changed the hit classname to containsShip

--- src/updateDomBoard.test.js
+++ src/updateDomBoard.test.js
@@ -6,5 +6,5 @@ player1.placeShip({ x: 1, y: 1 }, { x: 1, y: 2 });
 p1DomBoardManager.sendNextCollumn();
 const tempBox = getByTestId(`P1Box-1-1`);
 test("temp box with the id of 'P1Box-1-1' has the classname of hit", () => {
-    expect(p1DomBoardManager.checkForClass(tempBox, "hit")).toBe(true);
+    expect(p1DomBoardManager.checkForClass(tempBox, "containsShip")).toBe(true);
 })
