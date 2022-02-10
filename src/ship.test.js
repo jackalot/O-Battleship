79a202e85b07b126ab40e8917cd9bd6003c16d5a
jackalotUVM <gonzalezc9999@gmail.com>
2022-02-10 10:17:54 -0500

    renamed the first instance of partCoordinates in mockShip2

--- src/ship.test.js
+++ src/ship.test.js
@@ -43,7 +43,7 @@ test("mockShip.shipParts will return every part", () => {
        {"hasSunk": false, "partCoordinates": {"x": 1, "y": 7}, "partName": "part7"}]    );
 })
 test.only("mockShip2.shipParts will return every part", () => {
-    expect(mockShip2.shipParts()).toEqual([{"hasSunk": false, "partCoordinate": {"x": 9, "y": 3}, "partName": "part1"},
+    expect(mockShip2.shipParts()).toEqual([{"hasSunk": false, "partCoordinates": {"x": 9, "y": 3}, "partName": "part1"},
     {"hasSunk": false, "partCoordinates": {"x": 9, "y": 4}, "partName": "part2"},
     {"hasSunk": false, "partCoordinates": {"x": 9, "y": 5}, "partName": "part3"},
     {"hasSunk": false, "partCoordinates": {"x": 9, "y": 6}, "partName": "part4"},
