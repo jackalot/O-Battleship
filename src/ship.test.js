1c3f10e572e26768d3568f383440f93ee193e165
jackalotUVM <gonzalezc9999@gmail.com>
2022-02-10 08:35:17 -0500

    added a test for mockShip2 to return every part

--- src/ship.test.js
+++ src/ship.test.js
@@ -42,6 +42,15 @@ test("mockShip.shipParts will return every part", () => {
        {"hasSunk": false, "partCoordinates": {"x": 1, "y": 6}, "partName": "part6"}, 
        {"hasSunk": false, "partCoordinates": {"x": 1, "y": 7}, "partName": "part7"}]    );
 })
+test("mockSHip.shipParts will return every part", () => {
+    expect(mockShip2.shipParts()).toEqual([{"hasSunk": false, "partCoordinate": {"x": 9, "y": 3}, "partName": "part1"},
+    {"hasSunk": false, "partCoordinate": {"x": 9, "y": 4}, "partName": "part2"},
+    {"hasSunk": false, "partCoordinate": {"x": 9, "y": 5}, "partName": "part3"},
+    {"hasSunk": false, "partCoordinate": {"x": 9, "y": 6}, "partName": "part5"},
+    {"hasSunk": false, "partCoordinate": {"x": 9, "y": 7}, "partName": "part6"},
+    {"hasSunk": false, "partCoordinate": {"x": 9, "y": 8}, "partName": "part7"},
+    {"hasSunk": false, "partCoordinate": {"x": 9, "y": 9}, "partName": "part8"}])
+})
 test("mockShip will not sink", () => {
     expect(mockShip.sink()).toBe('I still have parts left');
 })
