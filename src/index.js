e343ac240fa8eab2de2d56f01ca6eca3e7aa6aec
jackalotUVM <gonzalezc9999@gmail.com>
2022-02-14 08:30:56 -0500

    added a p2BoardManager and statement to check that player2's board can update before continuing with any plans

--- src/index.js
+++ src/index.js
@@ -8,13 +8,16 @@ const domBoardFile = require('./updateDOMBoard');
 const playerOne = playerFile('player 1', 0);
 const playerTwo = playerFile('CPU', 1);
 const p1DomBoardManager = domBoardFile(playerOne, 'P1Box');
+const p2DomBoardManager = domBoardFile(playerTwo, 'P2Box');
 playerOne.ourBoard.placeShip({ x: 1, y: 1 }, { x: 1, y: 7 });
 playerTwo.ourBoard.placeShip({ x: 1, y: 1 }, { x: 1, y: 7 });
 playerOne.ourBoard.placeShip({ x: 2, y: 1 }, { x: 8, y: 1 });
 playerTwo.ourBoard.placeShip({ x: 2, y: 1 }, { x: 8, y: 1 });
 playerOne.ourBoard.placeShip({ x: 9, y: 3 }, { x: 9, y: 9 });
 playerTwo.ourBoard.placeShip({ x: 9, y: 3 }, { x: 9, y: 9 });
+
 p1DomBoardManager.sendNextCollumn();
+p2DomBoardManager.sendNextCollumn();
 //  This returns false, which it shouldn't as line 15 has a ship
 console.log(p1DomBoardManager.searchShip({ x: 9, y: 3 }));
 console.log(playerOne.ourBoard.myShips);
