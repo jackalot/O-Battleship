384cb4041baaa8d1938a966e2bcc175af669fa67
jackalotUVM <gonzalezc9999@gmail.com>
2022-02-14 10:31:41 -0500

    copied the plan psuedo code, pasted it into attackDomManager, corrected the exported function to attackDomManager()

---
+++ src/attackDomManager.js
@@ -0,0 +1,17 @@
+//    Manages all the attacks sent via clicking on the boards, whether that be player1
+//    or player2
+function attackDomManager(player1, p1DomBoardManager, player2, p2DomBoardManager) {
+  function recieveDomAttack(coordinate, attackingPlayer) {
+    if (attackingPlayer === player1) {
+      player2.ourboard.recieveAttack(coordinate);
+      p2DomBoardManager.sendNextCollumn();
+    } else if (attackingPlayer === player2) {
+      player1.ourboard.recieveAttack(coordinate);
+      p1DomBoardManager.sendNextCollumn();
+    }
+  }
+  return {
+    recieveDomAttack,
+  };
+}
+module.exports = attackDomManager;
