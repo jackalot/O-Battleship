6310b6f61aeb52001c3b424f8380a077dd75d6d4
jackalotUVM <gonzalezc9999@gmail.com>
2022-02-15 10:38:18 -0500

    attackDomManager.recieveDomAttack now has no if statements

--- src/attackDomManager.js
+++ src/attackDomManager.js
@@ -3,13 +3,8 @@
 function attackDomManager(player1, player2, callItAgain) {
   console.log(player1.ourboard);
   function recieveDomAttack(coordinate, attackingPlayer, defendingPlayer) {
-    if (attackingPlayer === player1) {
-      defendingPlayer.ourboard.recieveAttack(coordinate);
-      callItAgain();
-    } else if (attackingPlayer === player2) {
-      defendingPlayer.ourboard.recieveAttack(coordinate);
-      callItAgain();
-    }
+    defendingPlayer.ourboard.recieveAttack(coordinate);
+    callItAgain();
   }
   return {
     recieveDomAttack,
