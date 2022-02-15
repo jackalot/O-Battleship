1ede4d488c4c9cdd05ba2a64dec8a9098d7c445c
jackalotUVM <gonzalezc9999@gmail.com>
2022-02-15 10:19:19 -0500

    commented the console.logs in index.js, added a console.log in attackDomManager, turns out that it is only undefined in attackDomManager

--- src/attackDomManager.js
+++ src/attackDomManager.js
@@ -1,6 +1,7 @@
 //    Manages all the attacks sent via clicking on the boards, whether that be player1
 //    or player2
 function attackDomManager(player1, player2, callItAgain) {
+  console.log(player1.ourboard);
   function recieveDomAttack(coordinate, attackingPlayer) {
     if (attackingPlayer === player1) {
       player2.ourboard.recieveAttack(coordinate);
