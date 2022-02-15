6c9439b421c19be21db8c14f727a3b8cb60b9531
jackalotUVM <gonzalezc9999@gmail.com>
2022-02-15 09:43:11 -0500

    deleted two skip methods on the board.test.js file

--- src/board.test.js
+++ src/board.test.js
@@ -72,10 +72,10 @@ test("gameBoard checks if 2, 5 has a hit or miss on it already", () => {
     expect(mockBoard.checkAvailableCoord({x: 2, y: 5})).toBe(true);
 })
 // !needs fixing, skipping for now
-test.skip("gameBoard checks if a ship is at 1,1 and returns true as there is a ship there", () => {
+test("gameBoard checks if a ship is at 1,1 and returns true as there is a ship there", () => {
     expect(mockBoard.findFirstShip({x:1, y:1})).toBe(true);
 })
 // !needs fixing, skipping for now
-test.skip("gameBoard checks if a ship is at 9,3 and returns true as there is a ship there", () => {
+test("gameBoard checks if a ship is at 9,3 and returns true as there is a ship there", () => {
     expect(mockBoard.findFirstShip({x:9, y:3})).toBe(true);
 })
