d0c3e9be2e976406272233059b73312816667a1d
jackalotUVM <gonzalezc9999@gmail.com>
2022-02-17 09:21:15 -0500

    moved the searchShip space to 9 9

--- src/index.js
+++ src/index.js
@@ -30,5 +30,5 @@ playerTwo.ourBoard.placeShip({ x: 9, y: 3 }, { x: 9, y: 9 });
 
 p1DomBoardManager.sendNextCollumn();
 p2DomBoardManager.sendNextCollumn();
-console.log(p1DomBoardManager.searchShip({ x: 9, y: 3 }));
+console.log(p1DomBoardManager.searchShip({ x: 9, y: 9 }));
 console.log(playerOne.ourBoard.myShips);
