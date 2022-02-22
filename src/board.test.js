38c68f32e172d1c5d69e1652189b5c996ddfc883
jackalotUVM <gonzalezc9999@gmail.com>
2022-02-22 09:38:01 -0500

    fixed a typo in a comment

--- src/board.test.js
+++ src/board.test.js
@@ -77,7 +77,7 @@ test("gameBoard checks if a ship is at 9,3 and returns true as there is a ship t
 })
 /*
 The following test is tests for testing the limits of the board, like seeing if we could
-place ships there foer example
+place ships there for example
 */
 test("gameBoard has placed a ship from [10,1] to [10,10]", () => {
     expect(mockBoard.placeShip({x: 10, y: 1}, {x: 10, y: 10})).toMatchObject({ coord1: {x: 10, y: 1}, coord2: {x: 10, y: 10} });
