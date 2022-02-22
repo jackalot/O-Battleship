3642f515a60b3c4dd26c6e7599e6a0faf27a30cf
jackalotUVM <gonzalezc9999@gmail.com>
2022-02-22 08:51:49 -0500

    removed two comments that are no longer needed

--- src/board.test.js
+++ src/board.test.js
@@ -69,11 +69,9 @@ test("gameBoard checks if all ships have sunk, and returns false as theres still
 test("gameBoard checks if 2, 5 has a hit or miss on it already", () => {
     expect(mockBoard.checkAvailableCoord({x: 2, y: 5})).toBe(true);
 })
-// !needs fixing, skipping for now
 test("gameBoard checks if a ship is at 1,1 and returns true as there is a ship there", () => {
     expect(mockBoard.findFirstShip({x:1, y:1})).toBe(true);
 })
-// !needs fixing, skipping for now
 test("gameBoard checks if a ship is at 9,3 and returns true as there is a ship there", () => {
     expect(mockBoard.findFirstShip({x:9, y:3})).toBe(true);
 })
