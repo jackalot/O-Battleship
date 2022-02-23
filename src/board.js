a4e9b6c4c1d6534f3a2bc2d185cc7b0f68a9a84f
jackalotUVM <gonzalezc9999@gmail.com>
2022-02-23 09:19:06 -0500

    added two if statements to the for loop that checks if the coordinate equals the myships coordinate

--- src/board.js
+++ src/board.js
@@ -153,6 +153,11 @@ const gameBoardFactory = (bottomLeftCorner, topRightCorner) => {
   // finds the matching coordinate sent to it
   function findMatchingCoordinate(coord1, coord2) {
     for (let i = 0; i < myShips.length; i += 1) {
+      if (myShips.coord1 === coord1) {
+        if (myShips.coord2 === coord2) {
+
+        }
+      }
     }
   }
   function recieveAttack(coordinate) {
