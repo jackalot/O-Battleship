675d55289326a396dd48aa0c28946edb9de51610
jackalotUVM <gonzalezc9999@gmail.com>
2022-02-25 11:05:33 -0500

    ship.js now no longer has its console.logs

--- src/ship.js
+++ src/ship.js
@@ -87,9 +87,7 @@ const shipFactory = (coord1, coord2) => {
   // finds a given coordinate and see's if its in the coordinate Array from
   // getAllCoordinates
   function findCoordinate(guessCoord) {
-    console.log(guessCoord);
     const coordinateArray = getAllCoordinates();
-    console.log(coordinateArray);
     const failCase = -1;
     for (let i = 0; i < coordinateArray.length; i += 1) {
       if (coordinateArray[i].x === guessCoord.x) {
@@ -116,8 +114,6 @@ const shipFactory = (coord1, coord2) => {
   }
   function hit(guessCoord) {
     const result = findCoordinate(guessCoord);
-    console.log(result);
-    console.log(myParts);
     if (result === -1) {
       return false;
     }
