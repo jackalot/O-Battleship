480a8659fd498c9fdbbb05cb7b9b6ab3e9176406
jackalotUVM <gonzalezc9999@gmail.com>
2022-03-02 10:32:06 -0500

    after testing and logging the playerNumber thats returned, it seems that findPlayerNumber, returns the correct player

--- src/attackDomManager.js
+++ src/attackDomManager.js
@@ -28,6 +28,7 @@ function attackDomManager(player1, player2, callItAgain) {
   // will seperate the id into an object and will give each item
   function seperateIds(nonSeperatedId) {
     const playerNumber = findPlayerNumber(nonSeperatedId);
+    console.log(playerNumber);
     return {
       playerNumber,
     };
