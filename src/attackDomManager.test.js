a875b1b865601e426244c8aefda7495002e456ed
jackalotUVM <gonzalezc9999@gmail.com>
2022-03-02 10:10:07 -0500

    created a test without a return value, 'seperateIds will seperate the Id P1Box-10-10 into an object'

--- src/attackDomManager.test.js
+++ src/attackDomManager.test.js
@@ -3,3 +3,9 @@ const playerFile = require("./player");
 const player1 = playerFile("Player 1", 0);
 const player2 = playerFile("CPU", 1);
 const mockAttackDomManager = attackDomManagerFile(player1, player2);
+
+test("seperateIds will seperate the Id 'P1Box-10-10' into an object", () => {
+    mockAttackDomManager.seperateIds("P1Box-10-10").toMatchObject({
+        
+    })
+})
