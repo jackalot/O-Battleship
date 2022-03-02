8aba0f7d42cf6419a07d158ce5a21c85da248160
jackalotUVM <gonzalezc9999@gmail.com>
2022-03-02 09:06:13 -0500

    in the case of any immediate errors, added the psuedo code into attackDomManager

--- src/attackDomManager.js
+++ src/attackDomManager.js
@@ -2,12 +2,34 @@
 //    or player2
 function attackDomManager(player1, player2, callItAgain) {
   //  console.log(player1.ourboard);
-  function recieveDomAttack(coordinate, attackingPlayer, defendingPlayer) {
-    defendingPlayer.ourboard.recieveAttack(coordinate);
-    callItAgain();
+  function findUnconfirmedAttacks() {
+    const attackBox = document.querySelector('.unconfirmedAttack');
+    const playerString = attackBox.id[1];
+    const xString = attackBox.id[6];
+    const yString = attackBox.id[8];
+    const playerInt = parseInt(playerString);
+    const xInt = parseInt(xString);
+    const yInt = parseInt(yString);
+    const coordinate = { x: xInt, y: yInt };
+    if (playerInt === 1) {
+      recieveDomAttack(coordinate, playerOne, attackBox);
+    } else if (playerInt === 2) {
+      recieveDomAttack(coordinate, playerTwo, attackBox);
+    }
+  }
+  function recieveDomAttack(coordinate, defendingPlayer, domBox) {
+    const result = defendingPlayer.ourBoard.recieveAttack(coordinate);
+    console.log(result);
+    if (result === true) {
+      domBox.classList.remove('unconfirmedAttack');
+      domBox.classList.add('hit');
+    } else if (result === false) {
+      domBox.classList.remove('unconfirmedAttack');
+      domBox.classList.add('miss');
+    }
   }
   return {
-    recieveDomAttack,
+    findUnconfirmedAttacks,
   };
 }
 module.exports = attackDomManager;
