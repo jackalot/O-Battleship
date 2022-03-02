ef1d182690b9260bf3b623a693b83af91227b9e9
jackalotUVM <gonzalezc9999@gmail.com>
2022-03-02 10:05:50 -0500

    created a reference to attackDomManager, made a 'mockAttackDomManager'

--- src/attackDomManager.test.js
+++ src/attackDomManager.test.js
@@ -1,3 +1,5 @@
+const attackDomManagerFile = require('./attackDomManager');
 const playerFile = require("./player");
 const player1 = playerFile("Player 1", 0);
 const player2 = playerFile("CPU", 1);
+const mockAttackDomManager = attackDomManagerFile(player1, player2);
