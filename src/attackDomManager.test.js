2e45c55206fe8d35f3270f27bcfac1d3d541a2d8
jackalotUVM <gonzalezc9999@gmail.com>
2022-03-02 10:17:10 -0500

    added comments above all three tests to describe their use/edge case

--- src/attackDomManager.test.js
+++ src/attackDomManager.test.js
@@ -3,7 +3,7 @@ const playerFile = require("./player");
 const player1 = playerFile("Player 1", 0);
 const player2 = playerFile("CPU", 1);
 const mockAttackDomManager = attackDomManagerFile(player1, player2);
-
+// testing if double digit axis works, and if player 1 returns
 test("seperateIds will seperate the Id 'P1Box-10-10' into an object", () => {
     mockAttackDomManager.seperateIds("P1Box-10-10").toMatchObject({
         playerNumber: 1,
@@ -13,6 +13,7 @@ test("seperateIds will seperate the Id 'P1Box-10-10' into an object", () => {
         }
     })
 })
+// testing if single digit axis works on the x axis, and if player 2 returns
 test("seperateIds will seperate the Id 'P2Box-5-10' into an object", () => {
     mockAttackDomManager.seperateIds("P2Box-5-10").toMatchObject({
         playerNumber: 2,
@@ -22,6 +23,7 @@ test("seperateIds will seperate the Id 'P2Box-5-10' into an object", () => {
         }
     })
 })
+// testing if single digit axis works on the y axis, and if player 1 returns
 test("seperateIds will seperate the Id 'P1Box-10-5' into an object", () => {
     mockAttackDomManager.seperateIds("P2Box-10-5").toMatchObject({
         playerNumber: 1,
