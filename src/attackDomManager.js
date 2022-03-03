51528f55e4c3215f0ff061632a99a8542cfbf194
jackalotUVM <gonzalezc9999@gmail.com>
2022-03-03 08:18:43 -0500

    deleted most of the code that grabbed the id's from findUnconfirmedAttack, as we now have a function that does that

--- src/attackDomManager.js
+++ src/attackDomManager.js
@@ -80,20 +80,7 @@ function attackDomManager(player1, player2, callItAgain) {
   }
   function findUnconfirmedAttacks() {
     const attackBox = document.querySelector('.unconfirmedAttack');
-    //! Warning, this doesn't account for 10s, IE P1Box 10
-    //  gets the 1 or 2 in the player id, example:
-    // out of id: P1Box-6-7, it gets 1 after the P
-    const playerString = attackBox.id[1];
-    //  xString and y string get the coordinates from the id, example:
-    // out of id: P2Box-4-3, xString gets 4, yString gets 3
-    const xString = attackBox.id[6];
-    const yString = attackBox.id[8];
-    // parses all the strings and turns them into integers
-    // as they are currently strins and not integers
-    const playerInt = parseInt(playerString);
-    const xInt = parseInt(xString);
-    const yInt = parseInt(yString);
-    const coordinate = { x: xInt, y: yInt };
+
     if (playerInt === 1) {
       recieveDomAttack(coordinate, player1, attackBox);
     } else if (playerInt === 2) {
