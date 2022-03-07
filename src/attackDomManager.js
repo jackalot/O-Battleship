7ea78c4fc983630a0bec541f2398001f5b4ddb77
jackalotUVM <gonzalezc9999@gmail.com>
2022-03-07 10:07:26 -0500

    main.js update, whitespace changes in attackDomManager

--- src/attackDomManager.js
+++ src/attackDomManager.js
@@ -3,7 +3,7 @@
 function attackDomManager(player1, player2, p1DomBoardManager, p2DomBoardManager) {
   //  console.log(player1.ourboard);
   // recieveDomAttack() helperFunction
-  // calls p1DomBoardManager and p2DomBoardManager functions
+// calls p1DomBoardManager and p2DomBoardManager functions
   function swapVisibility(defendingPlayer) {
     if (defendingPlayer === player1) {
       p1DomBoardManager.swapHideShips();
