2628fa6357422fc94ffe60deb3144c18b45d7aad
jackalotUVM <gonzalezc9999@gmail.com>
2022-03-07 10:17:38 -0500

    removed a skip method from the sendAttack and sendRandomAttack tests

--- src/player.test.js
+++ src/player.test.js
@@ -8,7 +8,7 @@ test("player1 places a ship from [1, 1] to [1,7]", () => {
 test("player2 places a ship from [2, 2] to [2, 6]", () => {
     expect(player2.placeShip( {x: 2, y: 2}, {x: 2, y: 6})).toMatchObject({ coord1: {x: 2, y: 2}, coord2: {x: 2, y: 6} });
 })
-test.skip("player1 sends an attack to player2's board at [2,2] and it hits", () => {
+test("player1 sends an attack to player2's board at [2,2] and it hits", () => {
     expect(player1.sendAttack(player2, {x: 2, y: 2})).toBe(true);
 })
 //swapTurn tests will break if used with other tests, skip otherwise
@@ -19,6 +19,6 @@ test.skip("player1 will now have the player turn 1 (meaning its not their turn)"
     expect(player1.swapTurn()).toBe(1);
 })
 // true in this case means that after sending random attacks it managed to attack a valid coordinate
-test.skip("player2(CPU) sends a random attack to player1's board and it will return true", ()=> {
+test("player2(CPU) sends a random attack to player1's board and it will return true", ()=> {
 expect(player2.sendRandomAttack(player1)).toBe(true);
 })
