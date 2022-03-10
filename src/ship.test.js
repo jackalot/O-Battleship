26d725e971e8c6eb98bf1e0c62ad5438f81abe30
jackalotUVM <gonzalezc9999@gmail.com>
2022-03-10 11:07:20 -0500

    removed skip methods from the 'I still have parts' tests

--- src/ship.test.js
+++ src/ship.test.js
@@ -255,19 +255,19 @@ test("mockShip4 will not get hit at [9, 9]", () => {
     of the ships have completely sunken and they still have parts
     left.
 */
-test.skip("mockShip will not sink", () => {
+test("mockShip will not sink", () => {
     expect(mockShip.sink()).toBe('I still have parts left');
 })
-test.skip("mockShip2 will not sink", () => {
+test("mockShip2 will not sink", () => {
     expect(mockShip2.sink()).toBe('I still have parts left');
 })
-test.skip("horizontalShip will not sink", () => {
+test("horizontalShip will not sink", () => {
     expect(horizontalShip.sink()).toBe('I still have parts left');
 })
-test.skip("mockShip3 will not sink", () => {
+test("mockShip3 will not sink", () => {
     expect(mockShip3.sink()).toBe('I still have parts left');
 })
-test.skip("mockShip4 will not sink", () => {
+test("mockShip4 will not sink", () => {
     expect(mockShip4.sink()).toBe('I still have parts left');
 })
 /*
