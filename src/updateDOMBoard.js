1d94d3dd329cb1a7d958a2fae9cbcb0702ea3454
jackalotUVM <gonzalezc9999@gmail.com>
2022-03-14 09:25:21 -0400

    updateDomBoard.js is now returning hideShips

--- src/updateDOMBoard.js
+++ src/updateDOMBoard.js
@@ -118,7 +118,7 @@ function updateDOMBoard(myPlayer, searchQuery, otherPlayer, hideShips) {
     return false;
   }
   return {
-    sendNextCollumn, checkForClass, searchShip, searchMiss, searchHit, swapHideShips,
+    sendNextCollumn, checkForClass, searchShip, searchMiss, searchHit, swapHideShips, hideShips,
   };
 }
 module.exports = updateDOMBoard;
