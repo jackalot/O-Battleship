43b76d33cfe567175924591f3e2ababf4f55280f
jackalotUVM <gonzalezc9999@gmail.com>
2022-03-14 14:01:30 -0400

    added clearBoards from the psuedo code to the script

--- src/dropShips.js
+++ src/dropShips.js
@@ -1,4 +1,11 @@
 function dropShip() {
+  function clearBoards(playerCount) {
+    if (playerCount === 1) {
+      const playerOneBoard = document.querySelector('#playerOneBoard');
+      const allBoxes = playerOneBoard.findElementsByClassName('.box');
+      console.log(allBoxes);
+    }
+  }
   function askPlayerCount() {
     const windowResult = Number(window.prompt('How many players will be playing? For one player, please type 1, for two players, please type 2', ''));
     const result = parseInt(windowResult);
