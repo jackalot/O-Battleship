656ad8c6604c7f05a23e0feadca75a1b0fd81545
jackalotUVM <gonzalezc9999@gmail.com>
2022-03-14 11:27:28 -0400

    imported the current askPlayerCount to the dropShip sript

--- src/dropShips.js
+++ src/dropShips.js
@@ -1,4 +1,11 @@
 function dropShip() {
-
+  function askPlayerCount() {
+    const windowResult = Number(window.prompt('How many players will be playing? For one player, please type 1, for two players, please type 2', ''));
+    const result = parseInt(windowResult);
+    if (result === 1) {
+      console.log(result);
+    }
+  }
+  return { askPlayerCount };
 }
 module.exports = dropShip;
