bbf80b2316bfa1aeec51c4bdc1e0d4aee6454031
jackalotUVM <gonzalezc9999@gmail.com>
2022-04-01 08:43:11 -0400

    added different matchers to the tryEveryDirection test, it now has toEqual, arrayContaining, and objectContaining matchers

--- src/player.test.js
+++ src/player.test.js
@@ -43,8 +43,15 @@ const mockHit = {
 }
 test("When it comes to 1-1, player2 can only move up and right", () => {
     // toContain is a matcher for arrays, its to test if an array has a certain item
-    expect(player2.tryEveryDirection(mockHit, player2)).toContain({x: 1, y: 2 }, {x: 2, y: 1 });
-})
+    expect(player2.tryEveryDirection(mockHit, player2)).toEqual(
+        
+        expect.arrayContaining([
+            expect.objectContaining({x: 1, y: 2 }),
+            expect.objectContaining({x: 2, y: 1 })
+
+        ])
+    );
+});
 // true in this case means that after sending random attacks it managed to attack a valid coordinate
 test("player2(CPU) sends a random attack to player1's board and it will return true, it can either miss or hit", ()=> {
 expect(player2.sendRandomAttack(player1)).toBe(true);
