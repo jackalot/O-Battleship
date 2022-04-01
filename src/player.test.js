ac079ecaa170f104685d52dba967fc3048b82194
jackalotUVM <gonzalezc9999@gmail.com>
2022-04-01 09:39:33 -0400

    added a comment divider for testing the checkEachHitsFound function

--- src/player.test.js
+++ src/player.test.js
@@ -83,6 +83,9 @@ test("When it comes to 5-6, player2 can move up, left, right, and down", () => {
         ])
     );
 });
+/*
+    These will test checkEachHitsFound
+*/
 // true in this case means that after sending random attacks it managed to attack a valid coordinate
 test("player2(CPU) sends a random attack to player1's board and it will return true, it can either miss or hit", ()=> {
 expect(player2.sendRandomAttack(player1)).toBe(true);
