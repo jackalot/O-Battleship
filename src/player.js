62173616b3345fff7cdff43fdb4d2c21bc320406
jackalotUVM <gonzalezc9999@gmail.com>
2022-04-04 10:10:20 -0400

    added onto the findAdjacentSlot function helper function comment stating that tryEveryDirection call's that function

--- src/player.js
+++ src/player.js
@@ -45,7 +45,7 @@ const playerFactory = (playerName, playerTurn) => {
     }
     return false;
   }
-  //  sendRandomAttack() helper function()
+  //  sendRandomAttack() helper function(), tryEveryDirection() calls this function
   // recieves a coordinate and checks the coordinate nearby to see if it can be attacked
   function findAdjacentSlot(coordinate, moveByHowMuch, playerAttacking) {
     // moveByHowMuch will use this kind of coordinate system: x: 0 y: +2;
