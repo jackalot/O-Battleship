fb097c0d6b780b74981d3795e76ebcedb74d44c0
jackalotUVM <gonzalezc9999@gmail.com>
2022-04-05 10:22:16 -0400

    created a mockup of the hitWithGreatestNeighbors object

--- src/player.test.js
+++ src/player.test.js
@@ -133,6 +133,10 @@ test("Player 2 will check each of their last hits and see that 5, 6 has the most
 /*
     These are tests for the removeUndefinedDirections function
 */
+const mockHitWithGreatestNeighbors = {
+    ourCoordinates: { x: 0, y: 0 },
+    possibleDirections: [{ x: 0, y: 0 }, { x: 1, y: 2 }, { x: 2, y: 3 }, { x: 5, y: 4 }, undefined]
+  };
 // true in this case means that after sending random attacks it managed to attack a valid coordinate
 test("attack1, player2(CPU) sends a random attack to player1's board and it will return true, it can either miss or hit", ()=> {
 expect(player2.sendRandomAttack(player1)).toBe(true);
