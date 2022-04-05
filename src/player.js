2ad56e0435c8df24bf1d6efa7e196b17ca3b6245
jackalotUVM <gonzalezc9999@gmail.com>
2022-04-05 10:40:34 -0400

    in sendRandomAttack, we now call removeUndefinedDirection

--- src/player.js
+++ src/player.js
@@ -133,7 +133,8 @@ const playerFactory = (playerName, playerTurn) => {
     // in case it's a ship
     if (hitsFound.length > 0) {
       const placesWeCanHit = checkEachHitsFound(hitsFound, playerAttacking);
-      const attackResult = sendAttack(playerAttacking, placesWeCanHit[0]);
+      const possibleDirections = removeUndefinedDirections(placesWeCanHit);
+      const attackResult = sendAttack(playerAttacking, possibleDirections[0]);
       console.log(placesWeCanHit[0]);
       //  This is so the test could pass, the AI will not always hit
       return true;
